6efc8a2b51abf5f315180d04d80ce0803b35ee75
Thomas <thomas.lawanson@gmail.com>
2022-07-22 15:27:03 +0100

    Created Holding Page File

---
+++ src/components/holding-page.svelte
@@ -0,0 +1,46 @@
+<div class="div__holding-page-container">
+  <h1>HOLDING PAGE</h1>
+  <p>A show is going to happen.</p>
+  <div class="div__holding-page-link-container">
+    <a href="/about">[About]</a>
+    <a href="/schedule">[Live Events]</a>
+    <a href="https://goo.gl/maps/wXMErm3a1NbEAq1y5" target="_blank" rel="noref"
+      >[Location]</a
+    >
+    <a
+    href="https://www.eventbrite.co.uk/e/shivers-computational-arts-degree-show-mamfa-2021-tickets-164981515153"
+    target="_blank"
+    rel="noref"
+    class="parenthesis">[Ticketing]</a
+  >
+  </div>
+
+</div>
+
+<style>
+  a {
+    text-decoration-style: dotted;
+    cursor: pointer;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+
+  .div__holding-page-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .div__holding-page-link-container {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+  }
+  
+</style>
