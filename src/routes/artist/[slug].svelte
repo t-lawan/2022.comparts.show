9835c783aa94840fba7c3c46095f4184b50146a1
Thomas <thomas.lawanson@gmail.com>
2022-07-22 15:28:15 +0100

    Removed Floating blob logic

--- src/routes/artist/[slug].svelte
+++ src/routes/artist/[slug].svelte
@@ -16,23 +16,6 @@
   let history
   onMount(() => {
     history = window.history
-    anime({
-      targets: '#myClip .blob-path',
-      d: [
-        {
-          value:
-            'M95,-109.2C143.2,-72.7,216.4,-61.9,232.3,-32C248.2,-2.2,206.8,46.6,170.7,88.7C134.7,130.7,103.9,165.9,67.2,175C30.6,184.2,-12.1,167.3,-52.4,150C-92.8,132.7,-130.8,115.1,-141.6,86.6C-152.3,58,-135.7,18.5,-134,-29.3C-132.4,-77.1,-145.6,-133.1,-125.7,-174.6C-105.8,-216.1,-52.9,-243.1,-14.8,-225.5C23.4,-207.8,46.7,-145.7,95,-109.2Z',
-        },
-        {
-          value:
-            'M171.5,-191.9C215.3,-167.6,239.1,-107,234.1,-53.5C229,0.1,195,46.5,157.5,71.5C119.9,96.5,78.8,99.9,37.7,123.4C-3.3,146.9,-44.2,190.4,-72.4,185.9C-100.7,181.3,-116.1,128.7,-133.1,84.2C-150.1,39.7,-168.6,3.3,-177.6,-46.2C-186.7,-95.8,-186.2,-158.6,-155,-185.1C-123.8,-211.6,-61.9,-201.8,1,-202.9C63.9,-204.1,127.7,-216.2,171.5,-191.9Z',
-        },
-      ],
-      easing: 'easeInOutSine',
-      duration: 10000,
-      direction: 'infinite alternate',
-      loop: true,
-    })
   })
   const handleBackButton = () => {
     history.back()
@@ -81,23 +64,6 @@
         on:click={handleBackButton}
       />
       <div class="title">{artworkInfo.title}</div>
-      <svg preserveAspectRatio="none" viewBox="0 0 100 100">
-        <clipPath id="myClip">
-          <path
-            class="blob-path"
-            d="M171.5,-191.9C215.3,-167.6,239.1,-107,234.1,-53.5C229,0.1,195,46.5,157.5,71.5C119.9,96.5,78.8,99.9,37.7,123.4C-3.3,146.9,-44.2,190.4,-72.4,185.9C-100.7,181.3,-116.1,128.7,-133.1,84.2C-150.1,39.7,-168.6,3.3,-177.6,-46.2C-186.7,-95.8,-186.2,-158.6,-155,-185.1C-123.8,-211.6,-61.9,-201.8,1,-202.9C63.9,-204.1,127.7,-216.2,171.5,-191.9Z"
-            fill="#000"
-            style="transform: translateX(50px) translateY(50px) scale(0.2);"
-          />
-        </clipPath>
-        <g clip-path="url(#myClip)">
-          <image
-            href="./img/bios/{artistId}.jpg"
-            alt={artistId}
-            class="small-image"
-          />
-        </g>
-      </svg>
     </div>
     <Carousel perPage={1}>
       <span class="control" slot="left-control">
@@ -106,11 +72,24 @@
         {/if}
       </span>
       {#each artworkUrlArray as url, index}
-        <img
+        <!-- <img
           src="/img/artworks/{artistId}-artwork-{index}.jpg"
           alt="{artistId}-artwork-{index}.jpg"
           class="artwork-image"
-        />
+        /> -->
+
+        <!-- PLACE HOLDER -->
+        <img
+        src="/img/overlayImages/shivers_artist_00001.png"
+        alt="src"
+        class="artwork-image"
+      />
+      <img
+      src="/img/overlayImages/shivers_artist_00037.png"
+      alt="src"
+      class="artwork-image"
+    />
+
       {/each}
       <span class="control" slot="right-control">
         {#if artworkUrlArray.length > 1}
@@ -270,16 +249,6 @@
     background-color: rgba(255, 255, 255, 0.8);
     padding: 5px;
     border-radius: 100px;
-  }
-  svg {
-    width: 10vh;
-    height: 10vh;
-    margin-left: auto;
-    margin-right: 1vw;
-  }
-  .small-image {
-    width: 100%;
-    height: 100%;
   }
   @media (max-width: 768px) {
     .bio-image {
